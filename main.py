b82c3428546d226bbd9b1b0816e446e6bec98479
altradCS <145883008+altradCS@users.noreply.github.com>
2024-11-13 13:48:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-def hello():
-    print("hi")
+data = [1,2,4,6,2]
+target = 6
 
+import time
+# Binary Search
+def binary_search(data, target):
+    left, right = 0, len(data) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if data[mid] == target:
+            return mid
+        elif data[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
 
-def bye():
-    print("bye)
+# Perform search
+index = binary_search(data, target)
+print("Index of target:", index)
 
-
-print(hello())
